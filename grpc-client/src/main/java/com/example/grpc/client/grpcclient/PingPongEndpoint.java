4e72604d16a3d62ca99020392946db7841641d6c
Sergio <87139272+sirjoe777@users.noreply.github.com>
2022-04-13 18:59:14 +0100

    Once more trying upload

--- grpc-client/src/main/java/com/example/grpc/client/grpcclient/PingPongEndpoint.java
+++ grpc-client/src/main/java/com/example/grpc/client/grpcclient/PingPongEndpoint.java
@@ -6,8 +6,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.IOException;
@@ -27,5 +30,33 @@ public class PingPongEndpoint {
         @GetMapping("/add")
 	public String add() {
 		return grpcClientService.add();
+	}
+	@GetMapping("/")
+	public String home () {
+		return "uploadForm";
+	}
+	@PostMapping("/")
+	public String handleFileUpload(@RequestParam("file") MultipartFile file,RedirectAttributes redirectAttributes) throws IOException{
+
+			
+		System.out.println("I'M HERE");
+		// String data1 = new String(file.getBytes());
+		// Matrix matrix1=new Matrix(data1);
+
+		// String data2 = new String(file2.getBytes());
+		// Matrix matrix2=new Matrix(data2);
+
+		// checkEqual(matrix1, matrix2);
+		// if(operation.equals("Addition")){
+		// 	redirectAttributes.addFlashAttribute("result", grpcClientService.add(matrix1,matrix2));
+		// }
+		// else{
+		// 	redirectAttributes.addFlashAttribute("result", grpcClientService.multiply(matrix1,matrix2,deadline));
+		// }
+		
+		// redirectAttributes.addFlashAttribute("error","");
+		
+
+		return "redirect:/add";
 	}
 }
