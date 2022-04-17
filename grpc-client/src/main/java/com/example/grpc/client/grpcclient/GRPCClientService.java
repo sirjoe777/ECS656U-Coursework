df682f9d2c1445c4acb10c697ac430e2459ae0ac
Sergio <87139272+sirjoe777@users.noreply.github.com>
2022-04-17 18:33:32 +0100

    same as before

--- grpc-client/src/main/java/com/example/grpc/client/grpcclient/GRPCClientService.java
+++ grpc-client/src/main/java/com/example/grpc/client/grpcclient/GRPCClientService.java
@@ -50,10 +50,6 @@ public class GRPCClientService {
 		try{
 			String [] rows1 = string_matrix1.split("\n");
 			String [] rows2 = string_matrix2.split("\n");
-			System.out.println("printing rows1");
-			printArray(rows1);
-			System.out.println("printing rows2");
-			printArray(rows2);
 			//Check size is a power of 2
 			if (!checkPowerOfTwo(rows1.length) || !checkPowerOfTwo(rows2.length)){
 				redirectAttributes.addFlashAttribute("message", "Please make sure matrices' size is a power of 2.");
@@ -129,6 +125,8 @@ public class GRPCClientService {
 		try{
 			for (int row=0; row<SIZE; row++){
 				String [] row_entries = rows[row].split(",");
+				System.out.println("printing row "+row);
+				printArray(row_entries);
 				for (int column=0; column<SIZE; column++){
 					matrix[row][column] = Integer.parseInt(row_entries[column]);
 					//System.out.print(matrix[row][column]);
